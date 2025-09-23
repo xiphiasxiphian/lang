18ddd3a3ea6b89f8d831bbdf66fcfc3a9ed98b68
Sophie Holman <siaphix.siaphix@gmail.com>
2025-09-23 23:37:50 +0100

    array index tests

---
+++ src/frontend/parsers/lvalue.rs
@@ -0,0 +1,30 @@
+use nom::{branch::alt, character::complete::char, multi::{fold_many0, many0, many1}, sequence::delimited, Parser};
+
+use crate::frontend::{parsers::{common::{parse_ident}, expr::{parse_expr, Expr}, ParseResult, Span}, Ident};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum LValue
+{
+    Ident(Ident),
+    ArrayElem(Box<LValue>, Box<Expr>)
+}
+
+
+pub fn parse_lvalue(input: Span) -> ParseResult<LValue>
+{
+    println!("parse lvalue");
+    (
+        parse_ident,
+        many0(
+            delimited(
+                char('['),
+                parse_expr,
+                char(']')
+            )
+        )
+    )
+    .map(|(id, exs)|
+        exs.into_iter().fold(LValue::Ident(id), |x, y| LValue::ArrayElem(Box::new(x), Box::new(y)))
+    )
+    .parse(input)
+}
